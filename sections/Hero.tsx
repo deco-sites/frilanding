4a3b8b4424a6a08575b62ec7e509a78729059ff6
Rafael N <rfaeelnc@gmail.com>
2024-04-19 21:16:34 -0300

    feat(hero): anchor added

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -21,6 +21,7 @@ export interface Props {
   image?: ImageWidget;
   placement?: "left" | "right";
   cta?: CTA[];
+  anchor?: string;
 }
 
 const PLACEMENT = {
@@ -31,23 +32,23 @@ const PLACEMENT = {
 export default function HeroFlats({
   title = "Click here to tweak this text however you want.",
   description =
-    "This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.",
+  "This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.",
   image,
   placement = "left",
   cta = [
     { id: "change-me-1", href: "/", text: "Change me", outline: false },
     { id: "change-me-2", href: "/", text: "Change me", outline: true },
   ],
+  anchor = "#one"
 }: Props) {
   return (
     <nav class="lg:container lg:mx-auto mx-4">
       <div class="flex flex-col items-center gap-8">
         <div
-          class={`flex w-full xl:container xl:mx-auto py-20 mx-5 md:mx-10 z-10 ${
-            image
-              ? PLACEMENT[placement]
-              : "flex-col items-center justify-center text-center"
-          } lg:py-36 gap-12 md:gap-20 items-center`}
+          class={`flex w-full xl:container xl:mx-auto py-20 lg:pb-[2vw] mx-5 md:mx-10 z-10 ${image
+            ? PLACEMENT[placement]
+            : "flex-col items-center justify-center text-center"
+            } lg:py-36 gap-12 md:gap-20 items-center`}
         >
           {image && (
             <Image
@@ -61,11 +62,10 @@ export default function HeroFlats({
             />
           )}
           <div
-            class={`mx-6 lg:mx-auto lg:w-full space-y-4 gap-4 ${
-              image
-                ? "lg:w-1/2 lg:max-w-xl"
-                : "flex flex-col items-center justify-center lg:max-w-3xl"
-            }`}
+            class={`mx-6 lg:mx-auto lg:w-full space-y-4 gap-4 ${image
+              ? "lg:w-1/2 lg:max-w-xl"
+              : "flex flex-col items-center justify-center lg:max-w-3xl"
+              }`}
           >
             <div
               class="inline-block lg:text-[80px] text-4xl leading-none font-medium"
@@ -84,9 +84,8 @@ export default function HeroFlats({
                   id={item?.id}
                   href={item?.href}
                   target={item?.href.includes("http") ? "_blank" : "_self"}
-                  class={`font-normal btn btn-primary ${
-                    item.outline && "btn-outline"
-                  }`}
+                  class={`font-normal btn btn-primary ${item.outline && "btn-outline"
+                    }`}
                 >
                   {item?.text}
                 </a>
@@ -94,6 +93,13 @@ export default function HeroFlats({
             </div>
           </div>
         </div>
+        {anchor && (
+          <a href={anchor} class="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full items-center justify-center hidden lg:flex">
+            <svg class="w-6 h-6 text-white" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+              <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+            </svg>
+          </a>
+        )}
       </div>
     </nav>
   );
